c8c42138647d84187cbf6d24923e9588ff29598a
nickyy96 <nick.yarnall9@gmail.com>
2022-09-19 16:32:56 -0400

    FEAT: changed port

--- frontend/src/utils/globals.ts
+++ frontend/src/utils/globals.ts
@@ -2,4 +2,4 @@ export const NUM_ROWS = 6;
 export const WORD_LENGTH = 5;
 export const ALPHABET = "QWERTYUIOPASDFGHJKLZXCVBNM"
 export const FLIP_LENGTH = 250;
-export const URL = 'http://localhost:8080'
+export const URL = 'http://143.198.163.137:8080'
