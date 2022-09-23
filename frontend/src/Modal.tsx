38d4f6782f073e5e79c9881158aec2ba458e2ca2
nickyy96 <nick.yarnall9@gmail.com>
2022-09-23 01:49:43 -0400

    FIX: full phone compatibility

--- frontend/src/Modal.tsx
+++ frontend/src/Modal.tsx
@@ -103,8 +103,7 @@ const Modal = ({
                 <Stats/>
               )}
             </div>
-            {modal !== "win" && modal !== "lose" && (
-              <button className="close-modal" onClick={close}>
+            <button className="close-modal" onClick={close}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -115,8 +114,7 @@ const Modal = ({
                 >
                   <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
                 </svg>
-              </button>
-            )}
+            </button>
           </div>
         </div>
       )}
